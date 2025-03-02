d6eec819421985e4845872bcbefa20677abd94fd
Vicky <149935112+Vicky-258@users.noreply.github.com>
2025-03-02 15:05:11 +0530

    First Commit

---
+++ Easy/001_Two_sum.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> list[int] | None:
+
+        seen = {}
+
+        for i in range(len(nums)):
+            diff = target - nums[i]
+
+            if diff in seen:
+                return [seen[diff], i]
+
+            seen[nums[i]] = i
