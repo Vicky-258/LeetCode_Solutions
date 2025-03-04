1a66cfcbffd32e148e87711efc06376801bd7341
Vicky <149935112+Vicky-258@users.noreply.github.com>
2025-03-04 22:15:46 +0530

    Solved some Array problems

---
+++ Easy/136_Single_Number.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0
+
+        for i in nums:
+            res = res ^ i
+
+        return res
