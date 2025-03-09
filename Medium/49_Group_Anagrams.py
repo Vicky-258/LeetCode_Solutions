c04a37c6e55f90737111a6abd4470b6d774509c9
Vicky <149935112+Vicky-258@users.noreply.github.com>
2025-03-09 21:50:57 +0530

    Solved some problems

---
+++ Medium/49_Group_Anagrams.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dict1 = {}
+
+        for i in strs:
+
+            key = ''.join(sorted(i))
+
+            if key in dict1:
+                dict1[key].append(i)
+            else:
+                dict1[key] = [i]
+
+        return list(dict1.values())
